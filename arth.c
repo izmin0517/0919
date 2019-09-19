cbe1ec19f428f7a64de94598ef3480c6ac26d7ee
izmin0517 <55523883+izmin0517@users.noreply.github.com>
2019-09-19 15:09:31 +0900

    Add files via upload

---
+++ arth.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+	int a, b;
+	
+	printf("첫번째 수=");
+	scanf("%d",&a);
+	
+	printf("두번째 수=");
+	scanf("%d",&b);
+	
+	printf("%d + %d = %d\n", a, b, a+b);
+	printf("%d - %d = %d\n", a, b, a-b);
+	printf("%d * %d = %d\n",a, b, a*b);
+	printf("%d / %d = %f\n",a, b, (float)a/b);	
+}
